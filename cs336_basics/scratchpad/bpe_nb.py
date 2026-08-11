856e761d39ee1f65942b9c0566d78786dbad4f96
Christopher Stanek <chris.stanek97@gmail.com>
2026-08-11 15:30:12 +0200

    feat(pretokenization): milestone one chunk pre-tokenized

--- cs336_basics/scratchpad/bpe_nb.py
+++ cs336_basics/scratchpad/bpe_nb.py
@@ -14,7 +14,7 @@ def _():
     import logging
 
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format="%(levelname)s | %(name)s | %(message)s",
         force=True,
     )
@@ -46,7 +46,7 @@ def _(bpe, logger):
         new_processed_frequency_mapping = {}
         for tokens in processed_frequency_mapping:
             new_tokens = bpe.token_pass(tokens, max_pair, new_token)
-       
+   
             new_processed_frequency_mapping[tuple(new_tokens)] = processed_frequency_mapping[tokens]
             logger.debug(f"Updated mapping at {tuple(new_tokens)} to value {processed_frequency_mapping[tokens]}.")
 
@@ -58,7 +58,56 @@ def _(bpe, logger):
 
 
 @app.cell
+def _(logger):
+    from cs336_basics.pretokenization import find_chunk_boundaries
+    from pathlib import Path
+
+    test_file = Path("data/TinyStoriesV2-GPT4-valid.txt")
+
+    chunks = []
+    with open(test_file, 'rb') as f:
+        num_processes = 4
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            logger.debug(f"start:\t{start}, end:\t{end}, length [chars]: {(end-start)/1e6} M")
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            chunks.append(chunk)
+            logger.debug(f"Chunked text, first and last 100 chars:\n{chunk[:100]}\n...\n{chunk[-100:]}")
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
+    return (chunks,)
+
+
+@app.cell
 def _():
+    special_tokens = ["<|endoftext|>", "<|testtoken|>"]
+    "|".join(special_tokens)
+    return (special_tokens,)
+
+
+@app.cell
+def _(chunks, logger, special_tokens):
+    import regex as re
+    escaped_special_tokens = [re.escape(st) for st in special_tokens]
+    split_pattern = "|".join(escaped_special_tokens)
+    logger.debug(f"Using split pattern: {split_pattern} (| is regex or)")
+
+    matches = re.findall(split_pattern, chunks[0])
+    logger.debug(f"First 5 matches:\t{matches[:5]}")
+    logger.debug(f"Last 5 matches:\t{matches[-5:]}")
+
+    split_docs = re.split(split_pattern, chunks[0])
+    logger.debug(f"First 3 docs, first 20 chars:\t{split_docs[0][:20]}, {split_docs[1][:20]}, {split_docs[2][:20]}")
+
+    pretokenized_split_docs = []
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    for d in split_docs:
+        logger.debug(f"Pre-tokenizing:\t{d}")
+        pat_matches = re.findall(PAT, d)
+        logger.debug(pat_matches)
     return
 
 
